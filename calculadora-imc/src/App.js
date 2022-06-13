7d273f77b9975702ddf0d353ce5816634cefc875
Amanda Nascimento <87491868+AmandaJhes@users.noreply.github.com>
2022-06-13 12:52:11 -0300

    13/06

---
+++ calculadora-imc/src/App.js
@@ -0,0 +1,15 @@
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <div className={styles.main}>
+
+      <div className={styles.headerContainer}>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
